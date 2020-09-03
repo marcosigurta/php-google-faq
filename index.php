42c75d6ecfd86759597a5e92383536a8732f1b16
marco <marcosigurta.ovb@gmail.com>
2020-09-03 21:03:44 +0200

    Style Changes

--- index.php
+++ index.php
@@ -25,9 +25,9 @@ $data = [
         "anwser" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
 
         Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
-
+        
         Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
-
+        
         Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
     ],
     [
@@ -54,9 +54,16 @@ $data = [
 </head>
 
 <style>
+    body {
+        font-family: sans-serif;
+        width: 70%;
+        margin: 0 auto;
+    }
+
     .faq,
     .anwser {
         padding: 10px 5px;
+        text-align: center;
     }
 
     .faq {
